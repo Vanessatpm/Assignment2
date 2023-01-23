9e1e4b3f6bec73f8cafa06483c64b192aaae75e1
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-01-23 14:21:38 +0100

    feat: Added routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<StartupPage />} />
+          <Route path="/translation" element={<TranslationPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
