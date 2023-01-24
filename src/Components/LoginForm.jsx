60f948dd5a3734d31c6f81d290f3e4d236809aa0
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-01-24 22:00:49 +0100

    chore, wip: Moving fetching and related logic from reducer to loginForm

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,30 +1,44 @@
 import { useState } from "react";
-import { useDispatch, } from "react-redux";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import { getUser } from "./State/userReducer";
 
 function LoginForm() {
-    const [localUsername, setLocalUsername] = useState("");
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    
-    function handleLoginBtn() {
-        dispatch(getUser(localUsername));
-        navigate("/");
-    }
-    function updateLocalUsername(event){
-        setLocalUsername(event.target.value);
-    }
+  const [localUsername, setLocalUsername] = useState("");
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    return(
-        <>
-            <h4>{localUsername}</h4>
-            <form onSubmit={handleLoginBtn}>
-                <input type="text" onChange={updateLocalUsername} />
-                <button type="submit">Submit</button>
-            </form>
-        </>
-    )
+  function handleLoginBtn() {
+    const apiUrl = process.env.REACT_APP_API_URL;
+    fetch(`${apiUrl}?username=${localUsername}`)
+      .then((response) => response.json())
+      .then((results) => {
+        // array with users with the username
+        //Logger ut resultatet for å se hva man får
+        console.log(results);
+        //TODO: if result is an empty array: createUser and return the result of that call; else return one of the users?
+        //TODO: remove stuff from reducer
+      })
+      .catch((error) => {
+        //Logger ut error
+        console.log(error);
+      });
+    //dispatch(getUser(localUsername));
+    navigate("/");
+  }
+  function updateLocalUsername(event) {
+    setLocalUsername(event.target.value);
+  }
+
+  return (
+    <>
+      <h4>{localUsername}</h4>
+      <form onSubmit={handleLoginBtn}>
+        <input type="text" onChange={updateLocalUsername} />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default LoginForm;
--- src/Components/State/userReducer.js
+++ src/Components/State/userReducer.js
@@ -1,8 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { createHeaders } from "../../Api/index";
 
-const baseUrl = process.env.REACT_APP_API_URL;
-
 export const getUser = createAsyncThunk("user/getUser", async (username) => {
   const response = await fetch(`${baseUrl}?username=${username}`);
   if (response.ok) {
