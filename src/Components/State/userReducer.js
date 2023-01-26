2bf926cec5709711266ba888043e3ccbb18cb3dd
Vanessa Pastén-Millán <vane.tama@hotmail.com>
2023-01-26 08:59:04 +0100

    changes after merge conflict, I think..

--- src/Components/State/userReducer.js
+++ src/Components/State/userReducer.js
@@ -3,13 +3,9 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
   name: "user",
   initialState: {
-<<<<<<< HEAD
-    username: undefined
-=======
     id: undefined,
     username: undefined,
     translations: [],
->>>>>>> ea3a31e2e70b1e253b2b0079d3aa48a25fc2e176
   },
 
   reducers: {
